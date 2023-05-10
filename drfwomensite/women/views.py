09d8c38e108d67303055fbfff7b2fbadcf7c8e7f
dogekiller21 <dogekiller21@gmail.com>
2023-05-10 18:53:01 +0300

    Change APIView to ViewSet

--- drfwomensite/women/views.py
+++ drfwomensite/women/views.py
@@ -1,26 +1,18 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
-from rest_framework import generics
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import viewsets, mixins
+from rest_framework.viewsets import GenericViewSet
 
 from women.models import Women
 from women.serializers import WomenSerializer
 
 
-class WomenMixin:
+class WomenViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
-
-
-class WomenAPIList(WomenMixin, generics.ListCreateAPIView):
-    ...
-
-
-class WomenAPIUpdate(WomenMixin, generics.UpdateAPIView):
-    ...
-
-
-class WomenAPIDetailView(WomenMixin, generics.RetrieveUpdateDestroyAPIView):
-    ...
