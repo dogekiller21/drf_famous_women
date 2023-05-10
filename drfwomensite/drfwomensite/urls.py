9c29f63e6e49b22d4e6c4e5ce3ede83a2e42099c
dogekiller21 <dogekiller21@gmail.com>
2023-05-10 19:53:41 +0300

    Add requirements.txt, add token auth with djoser

--- drfwomensite/drfwomensite/urls.py
+++ drfwomensite/drfwomensite/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
 
 from rest_framework import routers
 
@@ -32,5 +31,7 @@ urlpatterns = [
     path("api/v1/women/", WomenAPIList.as_view()),
     path("api/v1/women/<int:pk>", WomenAPIUpdate.as_view()),
     path("api/v1/womendelete/", WomenAPIDestroy.as_view()),
+    path("api/v1/auth/", include("djoser.urls")),
+    re_path(r"^auth/", include("djoser.urls.authtoken")),
     # path("api/v1/", include(router.urls))
 ]
--- drfwomensite/women/views.py
+++ drfwomensite/women/views.py
@@ -1,15 +1,10 @@
-from typing import Any
-
 from django.shortcuts import render
-from rest_framework import viewsets, mixins, generics
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
-from women.models import Women, Category
-from women.permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
+from women.models import Women
+from women.permissions import IsAdminOrReadOnly
 from women.serializers import WomenSerializer
 
 
@@ -23,32 +18,9 @@ class WomenAPIList(WomenMixin, generics.ListCreateAPIView):
 
 
 class WomenAPIUpdate(WomenMixin, generics.RetrieveUpdateAPIView):
-    permission_classes = (IsOwnerOrReadOnly, )
+    permission_classes = (IsAuthenticated, )
+    # authentication_classes = (TokenAuthentication, )  # Auth only with token, not session
 
 
 class WomenAPIDestroy(WomenMixin, generics.RetrieveDestroyAPIView):
     permission_classes = (IsAdminOrReadOnly, )
-
-
-# class WomenViewSet(
-#     mixins.CreateModelMixin,
-#     mixins.RetrieveModelMixin,
-#     mixins.UpdateModelMixin,
-#     mixins.DestroyModelMixin,
-#     mixins.ListModelMixin,
-#     GenericViewSet
-# ):
-#     serializer_class = WomenSerializer
-#
-#     def get_queryset(self):
-#         pk = self.kwargs.get("pk")
-#         if pk is None:
-#             return Women.objects.all()
-#         return Women.objects.filter(pk=pk)
-#
-#     @action(methods=["get"], detail=False, url_path="category")
-#     def get_category(self, request: Request):
-#         categories = Category.objects.all()
-#         return Response(
-#             {"categories": [category.name for category in categories]}
-#         )
