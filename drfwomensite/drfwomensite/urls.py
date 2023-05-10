d6948a73c5c9b6eb30128f09c141c80d4ef6fdbd
dogekiller21 <dogekiller21@gmail.com>
2023-05-10 19:35:55 +0300

    Add session-bases auth

--- drfwomensite/drfwomensite/urls.py
+++ drfwomensite/drfwomensite/urls.py
@@ -28,6 +28,7 @@ from women.views import WomenAPIList, WomenAPIUpdate, WomenAPIDestroy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/session_auth/', include("rest_framework.urls")),
     path("api/v1/women/", WomenAPIList.as_view()),
     path("api/v1/women/<int:pk>", WomenAPIUpdate.as_view()),
     path("api/v1/womendelete/", WomenAPIDestroy.as_view()),
